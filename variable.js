c3615629023b6ef55dc2dd26ff5f473c81b2bef5
Neeraj912005 <pandeyneerajkakra@gmail.com>
2024-09-11 20:40:32 +0530

    Create variable.js

---
+++ variable.js
@@ -0,0 +1,15 @@
+const accountId=123456
+let accountEmail="pandeyneeraj@gmai.com"
+ var accountPassword="neeraj@2005"
+ accountCity="jaipur"
+ accountState="mp";
+// accountEmail=2 //not allowed
+ accountEmail="neeraj@gmail.com"
+//prefer not be use var
+//bacause of issue in block scope and functional scope
+
+ accountPassword="2121212"
+   accountCity = "jabalpur"
+  console.table([accountEmail, accountPassword,accountCity,accountState])
+ 
+ 
